08de4b34dccdd156bf38370652c2058a0e780255
el-moudni-hicham <hichamelmoudni7@gmail.com>
2023-05-11 00:49:38 +0100

    after adding frontend part

---
+++ frontend/e-commerce-app-angular/src/app/products/products.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  products:any;
+  errMessage! : string;
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void{
+    this.http.get("http://localhost:8890/INVENTORY-SERVICE/products").subscribe({
+        next : (data) => {
+          this.products = data;
+        },
+        error : (err) => {
+          this.errMessage = err;
+        }
+    });
+  }
+}
